8401d862d1adcd635334ec62353b3773124b56bd
DipuSarker <mr.d.sarker@gmail.com>
2021-02-18 18:43:32 +0600

    Add project files.

---
+++ ClothBazzarEcom.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClothBazzarEcom.Web.MvcApplication" Language="C#" %>
---
+++ ClothBazzarEcom.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClothBazzarEcom.Web.Startup))]
+namespace ClothBazzarEcom.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
